f602f70b22845f398f87480ac8ca91c3f10a23c1
Rafal Kozlowski <rafal.kozlowski@motorolasolutions.com>
2021-05-31 06:58:13 +0200

    Basic setup

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,10 +2,10 @@
 import '../styles/main.scss';
 
 // JS
-import './pagination';
-import './prevs-section';
-import './lazy-animation';
-import './lazy-images';
+// import './pagination';
+// import './prevs-section';
+// import './lazy-animation';
+// import './lazy-images';
 
 if (process.env.NODE_ENV !== 'production') {
   require('../index.pug')
