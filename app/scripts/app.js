f2d0afec2f8257e14c36312fce0c8ead4b435103
Rath-san <rim.kozlowski@gmail.com>
2021-07-14 11:32:57 +0200

    cinematic update 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,142 +24,6 @@ import 'lite-youtube-embed'
 // import Splitting from 'splitting'
 ;(() => {
   window.addEventListener('load', () => {
-    // // animate logo
-
-    // grab logo
-    const logo = document.querySelector('.svg-logo')
-    // split by elements
-    const elements = Array.from(logo.querySelectorAll('g > *'))
-    // add offset to element
-    elements.forEach((el, index) => {
-      el.style = `
-        --path-index: ${index};
-      `
-    })
-
-    // const showTextOnSlide
-    // const hideTextOnSlide = ()
-
-    const sliderOptions = {
-      interval: 5000,
-      keyboard: true,
-      ride: 'carousel',
-      pause: false
-    }
-
-    const slider = $('#carouselExamples').carousel(sliderOptions)
-    const sliderTitles = Array.from(
-      document.querySelectorAll(
-        '#carouselExamples .carousel__titles .carousel-title'
-      )
-    )
-    const sliderContents = Array.from(
-      document.querySelectorAll(
-        '#carouselExamples .carousel__contents .carousel-title'
-      )
-    )
-
-    const sliderElements = [...sliderTitles, ...sliderContents]
-
-    slider.on('slide.bs.carousel', function (e) {
-      // console.log(e.to)
-      sliderElements.forEach((i) => i.classList.remove('out'))
-
-      sliderContents[e.from].classList.add('out')
-      sliderContents[e.from].classList.remove('active')
-      sliderContents[e.to].classList.add('active')
-
-      sliderTitles[e.from].classList.add('out')
-      sliderTitles[e.from].classList.remove('active')
-      sliderTitles[e.to].classList.add('active')
-    })
-
-    const sliderOptionsCon = {
-      interval: 1,
-      keyboard: true,
-      ride: 'carousel',
-      pause: false
-    }
-
-    const sliderContinuous = $('#carouselContinuous').carousel(sliderOptionsCon)
-
-    $('#carouselContinuous .carousel-item').each(function () {
-      var minPerSlide = 4
-      var next = $(this).next()
-      if (!next.length) {
-        next = $(this).siblings(':first')
-      }
-      next.children(':first-child').clone().appendTo($(this))
-
-      for (var i = 0; i < minPerSlide; i++) {
-        next = next.next()
-        if (!next.length) {
-          next = $(this).siblings(':first')
-        }
-
-        next.children(':first-child').clone().appendTo($(this))
-      }
-    })
-    // carousel-item", "active", "carousel-item-left", value: "carousel-item active carousel-item-left"
-    window.addEventListener('resize', () => {
-      $('#carouselContinuous').carousel('next')
-    })
-    // const sliderProgress = $('.carousel-progress')
-
-    // const sliderProgressStart = () =>
-    //   sliderProgress.css({
-    //     transform: `scaleX(1)`,
-    //     transition: `transform ${sliderOptions.interval}ms ease-in-out`
-    //   })
-
-    // const sliderProgressReset = () =>
-    //   sliderProgress.css({
-    //     transform: `scaleX(0)`,
-    //     transition: 'none'
-    //   })
-
-    // slider.on('slide.bs.carousel', () => {
-    //   sliderProgressStart()
-    //   setTimeout(() => {
-    //     sliderProgressReset()
-    //   }, sliderOptions.interval)
-    // })
-
-    // setTimeout(() => {
-    //   document.body.classList.add('ready')
-    // }, 200)
-
-    // // text splitting
-    // const titles = Array.from(document.querySelectorAll('.section__title'))
-    // const subTitles = Array.from(
-    //   document.querySelectorAll('.section__subtitle')
-    // )
-    // const displays = Array.from(document.querySelectorAll('.section__display'))
-
-    // Splitting({
-    //   target: [...titles, ...subTitles],
-    //   by: 'words'
-    // })
-
-    // Splitting({
-    //   target: displays
-    // })
-
-    // const cbIn = (target) => {
-    //   target.closest('.section__head').dataset.visible = true
-    // }
-
-    // const cbOut = (target) => {
-    //   target.closest('.section__head').dataset.visible = false
-    // }
-
-    // doOnVisible({
-    //   sectionSelector: titles,
-    //   cbIn,
-    //   cbOut,
-    //   disconectOnIn: false,
-    //   threshold: 1
-    // })
 
     // // collapsible btn
     Array.from(document.querySelectorAll('.btn-collapse')).forEach((btn, i) => {
@@ -176,6 +40,8 @@ import 'lite-youtube-embed'
       const headerVideos = Array.from(header.querySelectorAll('video'))
 
       const headerComponents = [...headerImages, ...headerVideos]
+
+      // console.log(headerC);
 
       const componentsResolved = Promise.all(
         headerComponents.map(
